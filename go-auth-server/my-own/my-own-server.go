ce0864439dde5aa8a2444ecff15c110e0a5bf98f
sb.kim <sb.kim@kt.com>
2023-02-15 16:28:36 +0900

    feat: implement a structure of server handlers

--- go-auth-server/my-own/my-own-server.go
+++ go-auth-server/my-own/my-own-server.go
@@ -1,12 +1,15 @@
 package my_own
 
 import (
+	"context"
 	"github.com/go-oauth2/oauth2/v4"
+	"github.com/go-oauth2/oauth2/v4/errors"
 	"github.com/go-oauth2/oauth2/v4/generates"
 	"github.com/go-oauth2/oauth2/v4/manage"
 	"github.com/go-oauth2/oauth2/v4/models"
 	"github.com/go-oauth2/oauth2/v4/server"
 	"github.com/go-oauth2/oauth2/v4/store"
+	"net/http"
 	"time"
 )
 
@@ -77,4 +80,75 @@ func main() {
 		oauth2.Refreshing,
 	)
 
+	srv.SetClientInfoHandler(myClientInfoHandler)
+	srv.SetClientAuthorizedHandler(myClientAuthorizedHandler)
+	srv.SetClientScopeHandler(myClientScopeHandler)
+
+	srv.SetUserAuthorizationHandler(myUserAuthorizationHandler)
+	srv.SetPasswordAuthorizationHandler(myPasswordAuthorizationHandler)
+
+	srv.SetRefreshingScopeHandler(myRefreshingScopeHandler)
+	srv.SetRefreshingValidationHandler(myValidateURIHandler)
+
+	srv.SetResponseTokenHandler(myResponseTokenHandler)
+	srv.SetResponseErrorHandler(myResponseErrorHandler)
+
+	srv.SetInternalErrorHandler(myInternalErrorHandler)
+	srv.SetExtensionFieldsHandler(myExtensionFieldsHandler)
+	srv.SetAccessTokenExpHandler(myAccessTokenExpHandler)
+	srv.SetAuthorizeScopeHandler(myAuthorizeScopeHandler)
+}
+
+// TODO: implement handlers...
+
+func myClientInfoHandler(r *http.Request) (clientID, clientSecret string, err error) {
+	return server.ClientBasicHandler(r)
+}
+
+func myClientAuthorizedHandler(clientID string, grant oauth2.GrantType) (allowed bool, err error) {
+	return
+}
+
+func myClientScopeHandler(tgr *oauth2.TokenGenerateRequest) (allowed bool, err error) {
+	return
+}
+
+func myUserAuthorizationHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	return
+}
+
+func myPasswordAuthorizationHandler(ctx context.Context, clientID, username, password string) (userID string, err error) {
+	return
+}
+
+func myRefreshingScopeHandler(tgr *oauth2.TokenGenerateRequest, scope string) (allowed bool, err error) {
+	return
+}
+
+func myValidateURIHandler(ti oauth2.TokenInfo) (allowed bool, err error) {
+	return
+}
+
+func myResponseTokenHandler(w http.ResponseWriter, data map[string]interface{}, header http.Header, code ...int) error {
+	return nil
+}
+
+func myResponseErrorHandler(re *errors.Response) {
+	return
+}
+
+func myInternalErrorHandler(err error) (re *errors.Response) {
+	return
+}
+
+func myExtensionFieldsHandler(ti oauth2.TokenInfo) (fieldsValue map[string]interface{}) {
+	return
+}
+
+func myAccessTokenExpHandler(w http.ResponseWriter, r *http.Request) (exp time.Duration, err error) {
+	return
+}
+
+func myAuthorizeScopeHandler(w http.ResponseWriter, r *http.Request) (scope string, err error) {
+	return
 }
