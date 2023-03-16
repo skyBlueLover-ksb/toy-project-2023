0247d69d1be7d07bb3b1b99b67acaa56eb0f88d6
sb.kim <sb.kim@kt.com>
2023-03-16 14:48:49 +0900

    feat: some example client/server of EdgeCloud API using KeyClock

--- simpleAPI/edge_example_using_keycloak/api_server/server.go
+++ simpleAPI/edge_example_using_keycloak/api_server/server.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"bytes"
-"encoding/json"
-"fmt"
+	"encoding/json"
+	"fmt"
 	"io"
-	"io/ioutil"
-"log"
-"net/http"
+	"log"
+	"net/http"
 
-mux "github.com/gorilla/mux"
+	mux "github.com/gorilla/mux"
 )
 
 // Define constants for Keycloak server URL and credentials
@@ -28,7 +27,7 @@ var edgeSiteData = []map[string]interface{}{
 
 // Define a struct to hold the introspection response data
 type IntrospectionResponse struct {
-	Active bool `json:"active"`
+	Active bool     `json:"active"`
 	Roles  []string `json:"roles"` // Add a field to store roles from token claim
 }
 
@@ -53,30 +52,30 @@ func verifyBearerToken(token string) (bool, []string, error) { // Modify return 
 	// Send request and get response
 	resp, err := client.Do(req)
 	if err != nil {
-		return false, nil ,err // Modify return value to include roles
+		return false, nil, err // Modify return value to include roles
 	}
 	defer resp.Body.Close()
 
 	// Read response body as bytes
 	respBodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return false,nil ,err  // Modify return value to include roles
+		return false, nil, err // Modify return value to include roles
 	}
 
 	// Decode response body as JSON into IntrospectionResponse struct
 	var respData IntrospectionResponse
-	err = json.Unmarshal(respBodyBytes,&respData)
+	err = json.Unmarshal(respBodyBytes, &respData)
 	if err != nil {
-		return false,nil ,err  // Modify return value to include roles
+		return false, nil, err // Modify return value to include roles
 	}
 
 	// Return active field value and roles as verification result
-	return respData.Active ,respData.Roles ,nil  // Modify return value to include roles
+	return respData.Active, respData.Roles, nil // Modify return value to include roles
 
 }
 
 // Define a handler function to verify authentication token and return edge site data using introspection logic
-func handleEdgeSiteSelectionAndRouting(w http.ResponseWriter,r *http.Request) {
+func handleEdgeSiteSelectionAndRouting(w http.ResponseWriter, r *http.Request) {
 	// Get authentication token from request header
 	token := r.Header.Get("Authorization")
 
@@ -90,6 +89,64 @@ func handleEdgeSiteSelectionAndRouting(w http.ResponseWriter,r *http.Request) {
 
 	// Verify Bearer token using introspection logic
 	tokenString := token[7:]
-	isValidToken ,roles ,err := verifyBearerToken(tokenString)  // Get roles from verification result
+	isValidToken, roles, err := verifyBearerToken(tokenString) // Get roles from verification result
 
-	/
+	// Check if there is an error during token verification
+	if err != nil {
+		// Return 500 Internal Server Error if there is an error
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	// Check if token is valid
+	if !isValidToken {
+		// Return 401 Unauthorized error if token is not valid
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Unauthorized"))
+		return
+	}
+
+	// Check if the user has the required role to access edge site data
+	if !contains(roles, "edgeSiteAdmin") {
+		// Return 403 Forbidden error if user does not have the required role
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Forbidden"))
+		return
+	}
+
+	// Marshal edge site data into JSON format
+	edgeSiteDataBytes, err := json.Marshal(edgeSiteData)
+	if err != nil {
+		// Return 500 Internal Server Error if there is an error
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	// Write edge site data as response
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(edgeSiteDataBytes)
+}
+
+// Define a helper function to check if a slice of strings contains a specific string
+func contains(slice []string, s string) bool {
+	for _, str := range slice {
+		if str == s {
+			return true
+		}
+	}
+	return false
+}
+
+// Define the main function to start the server
+func main() {
+	// Create a new Gorilla Mux router
+	r := mux.NewRouter().StrictSlash(true)
+
+	// Register the edge site selection and routing handler function to the router
+	r.HandleFunc("/edge-site", handleEdgeSiteSelectionAndRouting)
+
+	// Start the server and log any errors
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
